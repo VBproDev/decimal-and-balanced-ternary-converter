6d4256122a201a01f22bff7e3552c44cc7984e23
VBproDev <proshowspeed@gmail.com>
2025-05-21 13:14:08 +0530

    fix for deployment problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vbprodev.github.io',
+  site: 'https://vbprodev.github.io/decimal-and-balanced-ternary-converter',
   base: '/decimal-and-balanced-ternary-converter',
 });
