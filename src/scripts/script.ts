5c626c6599b7077003a4b0655ef3ae74aaf4cb25
VBproDev <proshowspeed@gmail.com>
2025-05-22 13:25:28 +0530

    still fixing

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,6 +1,6 @@
 import { decimalToBalancedTernary, balancedTernaryToDecimal } from "./utils";
 
-const webWorker = new Worker(new URL('./web-worker.ts', import.meta.url));
+const webWorker = new Worker(new URL('./web-worker.ts'));
 const num = document.querySelector(".numeric-input") as HTMLInputElement;
 const base = document.querySelector(".numeric-base-select")!;
 const outputArea = document.querySelector(".conversion-result")!;
